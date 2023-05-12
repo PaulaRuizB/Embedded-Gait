3a4ca18f764ee9ab754d4d805453342e26234487
PaulaRuizB <pruizb@uma.es>
2023-05-12 17:22:23 +0200

    news

--- scripts/trt_conversions.sh
+++ scripts/trt_conversions.sh
@@ -1,7 +1,7 @@
 ## From TensorFlow to TensorRT
 
 # From tenforflow to keras saved model
-python3 convert_keras_to_onnx.py --model_path_gait /home/pruiz/Embedded-Gait-main/Embedded-Gait/models/model_gaitset.hdf5 --name_model gaitset_keras --model_path_save /home/pruiz/prueba_chema
+python3 convert_keras_to_onnx.py --model_path_gait /home/pruiz/Embedded-Gait-main/Embedded-Gait/models/model_2D_150.hdf5 --name_model model_2D_150 --model_path_save /home/pruiz/prueba_chema
 
 # From keras saved model to ONNX
-!python -m tf2onnx.convert --saved-model '/home/pruiz/prueba_chema/gaitset_keras' --output '/home/pruiz/prueba_chema/model-state-1300.onnx'
+python3 -m tf2onnx.convert --saved-model '/home/pruiz/prueba_chema/model_2D_150' --output '/home/pruiz/prueba_chema/model_2D_150.onnx'
